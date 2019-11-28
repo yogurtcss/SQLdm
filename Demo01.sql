5c9a49c6d05fb7f258fc426f4bc3ea94426b7757
yogurtcss <1806631955@qq.com>
2019-11-28 10:59:05 +0800

    upload

--- Demo01.sql
+++ Demo01.sql
@@ -3,7 +3,9 @@
  2.可使用空格和缩进来增强语句的可读性
  3.MySQL数据库的SQL语句不区分大小写，关键字建议使用大写
  4.三种注释：
-    * 单行注释 -- (注释内容) 或 # (注释内容)
+    * 单行注释 
+      (1) --(第2道短杠后至少跟一个空格符, 例如空格、tab、换行符等等) 注释内容 
+      (2) #(井号后可以不带空格, #后面直接跟的就是注释) 注释内容
     * 多行注释  /* 注释内容  
 */
 
@@ -25,10 +27,67 @@
  所以，把 末尾处*星/杠号 前的空格删掉，使首尾处的 /杠*星号、 *星/杠号 缩进相同，
  如此，这一大段多行注释就可正确地被 “折叠”。
 
-
-正常的git操作！
+ 约定：每个不同内容的 “多行注释” 之间，相隔两行！
 */
 
 
+/* 2019-11-28 09:58:03
+ SQL分类
+ 1.DDL：Data Definition Language 数据定义语言
+ 用来 定义数据库对象：数据库、表、列等；
+ 关键字 create、drop、alter等
+ 
+ 2.DML：Data Manipulation Language 数据操作语言
+ 用来 对数据库中【表】的数据进行 增删改；
+ 关键字 insert、delete、update等
 
-show databases;
+ 3.DQL：Data Query Language 数据查询语言
+ 用来 查询数据库中【表】的记录(数据)；
+ 关键字 select、where等
+
+ 4.DCL：Data Control Language 数据控制语言(了解)
+ 用来 定义数据库的访问权限和安全级别，及创建用户；
+ 关键字：GRANT REVOKE 等
+*/
+
+
+/* 回忆：原来我是在暑假时 通过 phpStudy 安装的 MySQL服务！！
+ 所有MySQL对应的本地文件，在 phpStudy文件夹\MySQL文件夹\data文件夹 下
+ 在VScode这里，我localhost 连接的就是 
+ phpStudy文件夹\MySQL文件夹\data文件夹 下的数据库
+ 
+ DDL：Data Definition Language 数据定义语言
+ 操作数据库：CRUD
+ C：Create创建
+ R：Retrieve查询
+ U：Update修改
+ D：Delete删除
+*/
+
+
+/* 查看所有数据库的名称 */
+-- show databases;  -- 单行注释，查看所有数据库的名称
+
+/* show create database <某个数据库名称>
+ 1.查看对应的数据库，在创建时指定的字符集；
+ 2.查看某个数据库的创建语句
+*/
+/* show create database mysql;  -- 在这里 名为mysql数据库，字符集为gbk */
+
+/* create database [if not exists-可选的] <dbName-你指定的数据库名称> [character set <charSet-你指定的字符集,如gbk,utf8> ]
+ [可选的条件1: 当不存在此数据库名dbName时,]   
+ [可选的条件2: 以你指定的字符集charSet进行创建 (如gbk或 utf8, 注意这里没有短杠-嗷!) ]
+ 创建指定名称dbName的数据库
+
+ exist  /ɪɡˈzɪst/ 存在
+
+*/
+/* create database db1;  -- 创建数据库db1,以后就用db1做实验 */
+/* show databases; */
+
+/* drop database <数据库名>
+ 删除指定名称的数据库  
+*/
+/* drop database db1; */
+
+
