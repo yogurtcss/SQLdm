ccf0ba5ec29827f33d5dc5293aeedd39af39f50c
yogurtcss <1806631955@qq.com>
2019-12-06 10:10:09 +0800

    upload

--- SQLtest50/P1-P10.sql
+++ SQLtest50/P1-P10.sql
@@ -8,7 +8,29 @@ use db50;
 /* 1. 查询 "01" 课程比 "02" 课程成绩高的学生的信息及课程分数
  注：这应该是 针对同一名学生：比较其"01" 课程比 "02" 课程成绩，
  若 此学生的 "01" 课程比 "02" 课程成绩高，就选出此学生的 信息及课程分数
+ 不会做！
 */
-select score 
-from SC
-where sid = "01";
+
+-- 参考答案！
+-- 思路：不是先选出某个学生sid，而是用 选出中间结果作为表，
+-- 然后在中间结果的表中进行(比较)操作
+
+-- SELECT *
+-- FROM 
+--     (SELECT * FROM  SC WHERE SC.cid="01") AS biao01,    -- 取出 所有01课程 作为表1
+--     (SELECT * FROM  SC WHERE SC.cid="02") AS biao02     -- 取出 所有02课程 作为表2
+-- WHERE 
+--     biao01.score > biao02.score AND 
+--     biao01.sid = biao02.sid;
+
+select *
+from Student
+where sid in(   -- 这里子查询返回的结果不只有1行，而是 一个集合
+    select biao01.sid
+        from
+            (select * from SC where SC.cid="01") as biao01,
+            (select * from SC where SC.cid="02") as biao02
+        where
+            biao01.score > biao02.score and
+            biao01.sid = biao02.sid
+)
