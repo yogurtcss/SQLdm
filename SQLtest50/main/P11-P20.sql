3a8b42b3c22c8d985db8de752573f845f4b2489f
yogurtcss <1806631955@qq.com>
2019-12-16 23:16:48 +0800

    upload

--- SQLtest50/main/P11-P20.sql
+++ SQLtest50/main/P11-P20.sql
@@ -21,17 +21,35 @@ from
     (select SC.sid, count(score) as rst from SC where score<60 group by sid ) as biao01;
 where biao01.rst>=2; */
 
+-- 中间结果：学号、平均分的表
+/* select sid, avg(score)
+from SC
+group by sid; */
+
+
 -- 最终的子查询
-select *
-from Student
-where sid in(
-    
-    select biao01.sid
-    from
-           ( select SC.sid,  count(score) as rst 
-             from SC 
-             where score<60 
-             group by sid
-           ) 
-           as biao01
-);
+
+-- 多次反复打开VS code，可使得 ctrl + / 的多行注释样式 在 -- 和 /* 之间变换！！
+-- 最好是：当  ctrl + / 的多行注释为 /* 时，再进入真正的打码！ 多行的 -- 真的太难受了！
+
+select Student.*, avg(score) as avgRst  -- 平均成绩
+from SC,  Student   -- 在SC、Student表中选
+where SC.sid in(
+
+            -- 选出不及格的同学的学号
+            ( select biao01.sid
+              from
+                  ( select SC.sid,  count(score) as rst 
+                    from SC 
+                    where score<60 
+                    group by sid
+                  ) 
+                  as biao01
+            )
+
+) and SC.sid = Student.sid  -- SC、Student表通过sid列来连接
+group by sid;
+
+
+
+-- 12. 检索 "01" 课程分数小于 60，按分数降序排列的学生信息
