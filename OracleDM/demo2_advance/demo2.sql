092cd3a7f7a990ae17cb8f08ee1051be92f9aadb
yogurtcss <1806631955@qq.com>
2020-02-01 18:10:07 +0800

    upload

--- OracleDM/demo2_advance/demo2.sql
+++ OracleDM/demo2_advance/demo2.sql
@@ -1,5 +1,9 @@
 /* 2020-02-01 18:07:11
  解决了vscode中 SQLdm提交不了的问题： 
- 因为这个文件夹OracleDM/DBFs/YOGURT1.DBF这文件超过100M
+ 因为这个文件夹OracleDM/DBFs/YOGURT1.DBF这数据库文件超过100M
  不能提交到GitHub上！！
+
+ 解决：时光倒流术，不断地进行“撤销上一次的更改”，
+ 把 OracleDM/DBFs/YOGURT1.DBF 这文件添加到 .gitignore (忽略提交的清单) 中
+ 然后才“合并冲突”、提交、推送成功的！
 */
