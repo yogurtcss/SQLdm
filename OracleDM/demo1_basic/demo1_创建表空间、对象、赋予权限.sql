3fb8429d0286df72def7d992663f6a2bb16e53d9
yogurtcss <54223488+yogurtcss@users.noreply.github.com>
2020-02-01 17:42:30 +0800

    upload

---
+++ OracleDM/demo1_basic/demo1_创建表空间、对象、赋予权限.sql
@@ -0,0 +1,28 @@
+-- 2020-02-01 10:54:37
+
+/* 创建表空间 
+ 此表空间yogurt1 将会在PLSQL13的 Tablespaces 下出现
+*/
+-- create tablespace yogurt1   -- 表空间名为yogurt1
+-- datafile 'H:\ProcExes\SQLFiles\SQLdm\OracleDM\DBFs\yogurt1.dbf'  -- 指定 此表空间生成的数据文件的真实地址
+-- size 100m      -- 初始时100M的大小
+-- autoextend on  -- 当指定的100M不够用时，自动增长大小
+-- next 10m;      -- 开启自动扩展后，每次扩展的步长10m，注意英文分号结尾
+
+-- drop tablespace yogurt1;  -- 删除表空间
+
+/* 创建用户
+ 此用户yo1 将会在PLSQL13的 Users 下出现 
+
+ Oracle数据库的特点：表和 “其他数据库对象” 都是存储在【用户】之下。
+*/
+-- create user yo1     -- 指定 用户名yo1
+-- identified by root  -- 指定 用户名的密码为root
+-- default tablespace yogurt1;  -- 指定 yo1的默认使用的表空间为 yogurt1
+
+/* 新创建的用户 yo1 没有任何权限，需要由超级管理员sys为yo1赋予角色(权限)
+  connect角色  ——基本权限
+  resource角色 ——开发人员的权限
+  dba角色      ——最高权限，等于sys
+*/
+-- grant dba to yo1;
---
+++ OracleDM/demo1_basic/demo3_select语句、delete语句.sql
@@ -0,0 +1,29 @@
+-- 2020-02-01 12:48:54
+
+-- select * from person;
+
+/* 插入字符数据时使用英文双引号报错：ORA-00984 列在此处不允许。
+ 报错原因：Oracle 不能识别双引号，默认字符串都采用单引号
+*/
+-- insert into person(pid,pname) values( 1,"小明" );  -- 报错：ORA-00984 列在此处不允许。
+
+/* 在yo1用户下操作person表，新增一条记录 
+ 没有手动commit
+
+ 而在超级管理员sys中，能看到person的新记录吗？-- sys能看到！
+ ---
+ DDL(数据定义语言) - Create、Alter、Drop 这些语句自动提交，无需用 Commit 提交。
+ DQL（数据查询语言）- Select 查询语句不存在提交问题。
+ DML(数据操纵语言) - Insert、Update、Delete 这些语句需要 Commit 才能提交。
+
+*/
+
+-- insert into person(pid,pname) values( 1,'小明' );  
+
+-- update person set pname='哈哈' where pid=1;
+-- commit;
+
+/* 三个“删除” */
+-- delete from person;  -- 不带 where字段 的delte：不删除表结构，删除表中全部记录
+-- drop table person;  -- 扔掉整个表：删除表的所有结构
+-- truncate table person;  -- 不删除表结构，只删除表数据
